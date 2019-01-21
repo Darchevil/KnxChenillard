c9ede47e2acb960524ae69862cf3b292abe39112
Yoann <yderneville@gmail.com>
2019-01-21 11:50:01 +0100

    Create Program.cs
    First program, connection to the knx system

---
+++ src/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using KNXLib;
+
+namespace ChenillardIot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Connection to the KNX 
+            //var connection = new KNXConnectionTunneling(remoteIP, remotePort, localIP, localPort);
+        }
+    }
+}
